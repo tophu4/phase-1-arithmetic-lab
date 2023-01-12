11eb9c69062e1aec39572b206845b77e649c46ed
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-01-12 01:53:27 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+
+
+// 1. multiply two num == 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// 2. random > 0
+const random = Math.round(Math.random() * 1000) + 1;
+
+// 3. modulo operator == 4
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+
+// 4. max == 20
+const max = Math.max(5,19,1,20,2);
